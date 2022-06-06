c0716106cc876c4c38678db0ca6b48ff9036951f
github_ayns789 <joly.a.idf@gmail.com>
2022-06-06 20:18:55 +0200

    add fetch json and photos for the landing page

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,21 @@
+let photographers = [];
     async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        
+        try{
+            let response = await fetch("../../data/photographers.json")
+                if(response.ok){
+                    let data = await response.json()
+                    photographers = data;
+                } else {
+                    console.error('retour du serveur : ', response.statut);
+                } 
+        } catch(e){
+            console.log(e);
+        }
+
+        console.log(photographers);
+
+        return photographers;
     }
 
     async function displayData(photographers) {
