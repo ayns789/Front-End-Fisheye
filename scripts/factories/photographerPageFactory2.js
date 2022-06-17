9ea746d6900a372f1f32f1e5e4dc485ec7e305f0
github_ayns789 <joly.a.idf@gmail.com>
2022-06-17 11:30:53 +0200

    up gesture of videos

--- scripts/factories/photographerPageFactory2.js
+++ scripts/factories/photographerPageFactory2.js
@@ -1,7 +1,8 @@
 // import PhotographerWork from '../classes/PhotographerWork.js';
 
 export function photographerPageFactory2( photographie ) {
-    let dataPhotographer = JSON.parse(localStorage.getItem("photographerInfo"));
+    // let dataPhotographer = JSON.parse(localStorage.getItem("photographerInfo"));
+    
     let objPhotographie = {
         idPhotographie : photographie.id,
         idPhotographer : photographie.idPhotographer,
@@ -11,17 +12,26 @@ export function photographerPageFactory2( photographie ) {
         date : photographie.date,
         pricePhotographie : photographie.price
     }
-
+    let objPhotographieVideo = {
+        idPhotographie : photographie.id,
+        idPhotographer : photographie.idPhotographer,
+        title : photographie.title,
+        video : photographie.video, 
+        likes : photographie.likes,
+        date : photographie.date,
+        pricePhotographie : photographie.price
+    }
     
     const picture = `assets/photographies/${objPhotographie.image}`;
+    const movie = `assets/photographies/${objPhotographieVideo.video}`;
 
     function getUserCardDOM2() {
         
         // create elements html :
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
         const video = document.createElement( 'video' );
         const source = document.createElement( 'source' );
+        const img = document.createElement( 'img' );
         const h2 = document.createElement( 'h2' );
         // const h3 = document.createElement( 'h3' );
         // const p1 = document.createElement( 'p' );
@@ -37,46 +47,27 @@ export function photographerPageFactory2( photographie ) {
     }
   }
 
-  // function argumentsContientMp4(){
-  //   return [].includes.call(arguments, 'mp4');
-  // }
-  // let videoDetect = "mp4";
-  // let matches = picture.matchAll(videoDetect);
-
-  // if(matches){
-  // if(videoDetect.match(picture)){
-  // if(!videoDetect.test(picture)){
-  if(picture.includes("mp4")){
-  // if(argumentsContientMp4(picture)){
-    // alert('Word Match');
+  if(objPhotographieVideo.video){
     setAttributes(source, {
-      "src": picture, 
+      "src": movie, 
       "type": "video/mp4",
-      "aria-label": "le nom de la video est " + picture
+      "aria-label": "le nom de la video est " + objPhotographieVideo.title
     });
+    setAttributes(video, {
+      "width": "300", 
+      "height": "340",
+      "controls": "controls"
+    });
+
     video.appendChild(source);
     article.appendChild(video);
 
-    // setAttributes(img, {
-    //   "src": picture, 
-    //   "aria-label": "le nom de la photo est " + objPhotographie.title
-    // });
-    // article.appendChild(img);
-
-  } else {
-    // alert('Word not Match');
-    // setAttributes(source, {
-    //   "src": picture, 
-    //   "type": "video/mp4",
-    //   "aria-label": "le nom de la video est " + objPhotographie.title
-    // });
-    // video.appendChild(source);
-    // article.appendChild(video);
-
+  } else  {
     setAttributes(img, {
       "src": picture, 
-      "aria-label": "le nom de la photo est " + picture
+      "aria-label": "le nom de la photo est " + objPhotographie.title
     });
+    
     article.appendChild(img);
   }
           
